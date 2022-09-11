cbc26e51e1a0705f5a909d61ab4d2845b51a14d9
mahendrasaikumargandham <mahendragandham730@gmail.com>
2022-09-11 12:03:48 +0530

    Added PWA Code - serviceWorker

---
+++ public/serviceWorker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+this.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+this.addEventListener("activate", (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cachesName) =>
+      Promise.all(
+        cachesName.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
